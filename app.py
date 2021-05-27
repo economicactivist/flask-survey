d85615d6cacaacebe3488c1462cb5f9a7bad1e37
economicactivist <mountainscott67@yahoo.com>
2021-05-26 23:10:55 -0700

    about to start step six...may need to rename variables

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
-from flask import Flask, render_template, redirect, templating
+from flask import Flask, render_template, redirect, request
+from flask.helpers import url_for
 from surveys import satisfaction_survey
 from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secretkey'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 toolbar = DebugToolbarExtension(app)
 
 responses = []
-num = 0
+num = 0  # variable to set page number
 
 
 @app.route('/')
@@ -19,12 +21,18 @@ def index():
 
 @app.route('/questions/<num>')
 def questions(num):
-    num = int(num)
+    num = int(num)  # change string to integer
     if num < len(satisfaction_survey.questions):
         q = satisfaction_survey.questions[num]
         num += 1
-        return render_template('questions.html', question=q.question, num=num)
+        return render_template('questions.html', question=q.question, choices=q.choices, num=num)
     else:
-        return '<h1>Thank You!</h1>'
+        return '<body><h1>Thank You!</h1></body>'
 # if __name__ == '__main__':
 #   app.run(host='127.0.0.1', port=8000, debug=True)
+@app.route('/answer/<num>', methods=['POST'])
+def save_answer(num):
+    answer= request.form
+    responses.append(answer['answer'])
+    print(responses)
+    return redirect(url_for('questions', num=num))
